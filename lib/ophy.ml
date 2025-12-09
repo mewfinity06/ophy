0cfa87d268b414b7b3f11815214cf33ed25ad321
Ashton Sickler <ashton.sickler06@gmail.com>
2025-12-09 06:03:45 -0600

    Doc and reordering

--- lib/ophy.ml
+++ lib/ophy.ml
@@ -1,114 +1,132 @@
-(* Types *)
-type point =
-  { x : float
-  ; y : float
-  }
+module Point = struct
+  type t =
+    { x : float
+    ; y : float
+    }
 
-type circle =
-  { pos : point
-  ; radius : float
-  }
+  let distance (p1 : t) (p2 : t) =
+    let dist_x = p1.x -. p2.x in
+    let dist_y = p1.y -. p2.y in
+    sqrt ((dist_x *. dist_x) +. (dist_y *. dist_y))
+  ;;
+end
 
-type rect =
-  { pos : point
-  ; width : float
-  ; height : float
-  }
+module Circle = struct
+  type t =
+    { pos : Point.t
+    ; radius : float
+    }
+end
 
-type line =
-  { p1 : point
-  ; p2 : point
-  }
+module Rect = struct
+  type t =
+    { pos : Point.t
+    ; width : float
+    ; height : float
+    }
+end
 
-(* Functions *)
-let distance p1 p2 =
-  let dist_x = p1.x -. p2.x in
-  let dist_y = p1.y -. p2.y in
-  sqrt ((dist_x *. dist_x) +. (dist_y *. dist_y))
-;;
+module Line = struct
+  type t =
+    { p1 : Point.t
+    ; p2 : Point.t
+    }
+end
 
-let is_collision_p2p (p1 : point) (p2 : point) = p1 = p2
-let is_collision_p2c (p1 : point) (c1 : circle) = distance p1 c1.pos <= c1.radius
+(** 
+    The Collide module provides all the necessary functions to check
+    if one type has collided with the other, i.e. {!Collide.p2p} checks if 
+    a {!Point.t} has collided with another and {!Collide.l2c} checks if a 
+    {!Line.t} collides with a {!Circle.t}. These functions are based off 
+    of Jeffrey Thompson's
+    {{:https://www.jeffreythompson.org/collision-detection/}Collision Detection}
+*)
+module Collide = struct
+  let p2p (p1 : Point.t) (p2 : Point.t) = p1 = p2
+  let p2c (p1 : Point.t) (c1 : Circle.t) = Point.distance p1 c1.pos <= c1.radius
 
-let is_collision_c2c (c1 : circle) (c2 : circle) =
-  let dist_center = distance c1.pos c2.pos in
-  dist_center <= c1.radius +. c2.radius
-;;
+  let c2c (c1 : Circle.t) (c2 : Circle.t) =
+    Point.distance c1.pos c2.pos <= c1.radius +. c2.radius
+  ;;
 
-let is_collision_p2r (p1 : point) (r1 : rect) =
-  let { x = px; y = py } = p1 in
-  let { pos = rp; width = rw; height = rh } = r1 in
-  px >= rp.x && rp.x <= rp.x +. rw && py >= rp.y && py <= rp.y +. rh
-;;
+  let p2r (p1 : Point.t) (r1 : Rect.t) =
+    let open Point in
+    let open Rect in
+    let { x = px; y = py } = p1 in
+    let { pos = rp; width = rw; height = rh } = r1 in
+    px >= rp.x && rp.x <= rp.x +. rw && py >= rp.y && py <= rp.y +. rh
+  ;;
 
-let is_collision_r2r (r1 : rect) (r2 : rect) =
-  let { pos = { x = r1x; y = r1y }; width = r1w; height = r1h } = r1 in
-  let { pos = { x = r2x; y = r2y }; width = r2w; height = r2h } = r2 in
-  r1x +. r1w >= r2x && r1x <= r2x +. r2w && r1y +. r1h >= r2y && r1y <= r2y +. r2h
-;;
+  let r2r (r1 : Rect.t) (r2 : Rect.t) =
+    let open Rect in
+    let { pos = { x = r1x; y = r1y }; width = r1w; height = r1h } = r1 in
+    let { pos = { x = r2x; y = r2y }; width = r2w; height = r2h } = r2 in
+    r1x +. r1w >= r2x && r1x <= r2x +. r2w && r1y +. r1h >= r2y && r1y <= r2y +. r2h
+  ;;
 
-let is_collision_c2r (c : circle) (r : rect) =
-  let test_x = ref 0.0 in
-  let test_y = ref 0.0 in
-  if c.pos.x < r.pos.x
-  then test_x := r.pos.x
-  else if c.pos.x > r.pos.x +. r.width
-  then test_x := r.pos.x +. r.width;
-  if c.pos.y < r.pos.y
-  then test_y := r.pos.y
-  else if c.pos.y > r.pos.y +. r.height
-  then test_y := r.pos.y +. r.height;
-  let dist_x = c.pos.x -. !test_x in
-  let dist_y = c.pos.y -. !test_y in
-  let dist = sqrt ((dist_x *. dist_x) +. (dist_y *. dist_y)) in
-  dist <= c.radius
-;;
+  let c2r (c : Circle.t) (r : Rect.t) =
+    let test_x = ref 0.0 in
+    let test_y = ref 0.0 in
+    if c.pos.x < r.pos.x
+    then test_x := r.pos.x
+    else if c.pos.x > r.pos.x +. r.width
+    then test_x := r.pos.x +. r.width;
+    if c.pos.y < r.pos.y
+    then test_y := r.pos.y
+    else if c.pos.y > r.pos.y +. r.height
+    then test_y := r.pos.y +. r.height;
+    let dist_x = c.pos.x -. !test_x in
+    let dist_y = c.pos.y -. !test_y in
+    let dist = sqrt ((dist_x *. dist_x) +. (dist_y *. dist_y)) in
+    dist <= c.radius
+  ;;
 
-let is_collision_l2p_buffer (b : float) (l : line) (p : point) =
-  let d1 = distance p l.p1 in
-  let d2 = distance p l.p2 in
-  let line_len = distance l.p1 l.p2 in
-  d1 +. d2 >= line_len -. b && d1 +. d2 <= line_len +. b
-;;
+  let l2p_buffer (b : float) (l : Line.t) (p : Point.t) =
+    let d1 = Point.distance p l.p1 in
+    let d2 = Point.distance p l.p2 in
+    let len = Point.distance l.p1 l.p2 in
+    d1 +. d2 >= len -. b && d1 +. d2 <= len +. b
+  ;;
 
-let is_collision_l2p = is_collision_l2p_buffer 0.1
+  let l2p = l2p_buffer 0.1
 
-let is_collision_l2c (l : line) (c : circle) =
-  let inside_1 = is_collision_p2c l.p1 c in
-  let inside_2 = is_collision_p2c l.p2 c in
-  if inside_1 || inside_2
-  then true
-  else (
-    let line_len = distance l.p1 l.p2 in
-    let dot =
-      (((c.pos.x -. l.p1.x) *. (l.p2.x -. l.p1.x))
-       +. ((c.pos.y -. l.p1.y) *. (l.p2.y -. l.p1.y)))
-      /. (line_len ** 2.)
-    in
-    let closest_x = l.p1.x +. (dot *. (l.p2.x -. l.p1.x)) in
-    let closest_y = l.p1.y +. (dot *. (l.p2.y -. l.p1.y)) in
-    let on_seg = is_collision_l2p l { x = closest_x; y = closest_y } in
-    if not on_seg
-    then false
+  let l2c (l : Line.t) (c : Circle.t) =
+    let inside_1 = p2c l.p1 c in
+    let inside_2 = p2c l.p2 c in
+    if inside_1 || inside_2
+    then true
     else (
-      let dist_x = closest_x -. c.pos.x in
-      let dist_y = closest_y -. c.pos.y in
-      let dist = sqrt ((dist_x *. dist_x) +. (dist_y *. dist_y)) in
-      dist <= c.radius))
-;;
+      let len = Point.distance l.p1 l.p2 in
+      let dot =
+        (((c.pos.x -. l.p1.x) *. (l.p2.x -. l.p1.x))
+         +. ((c.pos.y -. l.p1.y) *. (l.p2.y -. l.p1.y)))
+        /. (len ** 2.)
+      in
+      let closest_x = l.p1.x +. (dot *. (l.p2.x -. l.p1.x)) in
+      let closest_y = l.p1.y +. (dot *. (l.p2.y -. l.p1.y)) in
+      let on_seg = l2p l { x = closest_x; y = closest_y } in
+      if not on_seg
+      then false
+      else (
+        let dist_x = closest_x -. c.pos.x in
+        let dist_y = closest_y -. c.pos.y in
+        let dist = sqrt ((dist_x *. dist_x) +. (dist_y *. dist_y)) in
+        dist <= c.radius))
+  ;;
 
-let is_collision_l2l (l1 : line) (l2 : line) =
-  let uA =
-    (((l2.p2.x -. l2.p1.x) *. (l1.p1.y -. l2.p1.y))
-     -. ((l2.p2.y -. l2.p1.y) *. (l1.p1.x -. l2.p1.x)))
-    /. (((l2.p2.y -. l2.p1.y) *. (l1.p2.x -. l1.p1.x))
-        -. ((l2.p2.x -. l2.p1.x) *. (l1.p2.y -. l1.p1.y)))
-  in
-  let uB =
-    (((l1.p2.x -. l1.p1.x) *. (l1.p1.y -. l2.p1.y))
-     -. ((l1.p2.y -. l1.p1.y) *. (l1.p1.x -. l2.p1.x)))
-    /. (((l2.p2.y -. l2.p1.y) *. (l1.p2.x -. l1.p1.x))
-        -. ((l2.p2.x -. l2.p1.x) *. (l1.p2.y -. l1.p1.y)))
-  in
-  uA >= 0. && uA <= 1. && uB >= 0. && uB <= 1.
-;;
+  let l2l (l1 : Line.t) (l2 : Line.t) =
+    let uA =
+      (((l2.p2.x -. l2.p1.x) *. (l1.p1.y -. l2.p1.y))
+       -. ((l2.p2.y -. l2.p1.y) *. (l1.p1.x -. l2.p1.x)))
+      /. (((l2.p2.y -. l2.p1.y) *. (l1.p2.x -. l1.p1.x))
+          -. ((l2.p2.x -. l2.p1.x) *. (l1.p2.y -. l1.p1.y)))
+    in
+    let uB =
+      (((l1.p2.x -. l1.p1.x) *. (l1.p1.y -. l2.p1.y))
+       -. ((l1.p2.y -. l1.p1.y) *. (l1.p1.x -. l2.p1.x)))
+      /. (((l2.p2.y -. l2.p1.y) *. (l1.p2.x -. l1.p1.x))
+          -. ((l2.p2.x -. l2.p1.x) *. (l1.p2.y -. l1.p1.y)))
+    in
+    uA >= 0. && uA <= 1. && uB >= 0. && uB <= 1.
+  ;;
+end
